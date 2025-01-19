ad1984fc7d0827f105567d0590694515dc994780
Serent <Serent2003@mail.ru>
2025-01-19 15:52:29 +0300

    feat(sessionsPage): add page for sessions, create button, pagination

---
+++ src/pages/SessionsPage.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { getSessions } from 'src/services/sessionsService';
+import SessionList from 'components/SessionList.vue';
+import CreateSessionButton from 'components/button/CreateSessionButton.vue';
+
+const sessions = ref([]);
+const currentPage = ref(1);
+// const connectedServerAPI = ref(null);
+const totalSessions = ref(0);
+const sessionsPerPage = 5; // Количество сессий на странице
+
+const fetchSessions = async () => {
+  try {
+    const data = await getSessions(currentPage.value, sessionsPerPage);
+    sessions.value = data.sessions;
+    totalSessions.value = data.total;
+  } catch (error) {
+    console.error('Ошибка при загрузке сессий:', error);
+  }
+};
+
+// const onConnect = async () =>{
+//   // получаем последний url сервера
+//   const serverAPI = new TRSAPI("http://localhost:5000");
+//   const status = await serverAPI.getStatus()
+//   if (status) {
+//     rou
+//   }
+// }
+onMounted(() => {
+  fetchSessions();
+});
+</script>
+
+<template>
+  <div class="column">
+    <!-- Вставляем компонент кнопки создания сессии -->
+    <CreateSessionButton />
+
+    <div v-if="sessions.length" class="col self-center">
+      <SessionList :sessions="sessions" :totalSessions="totalSessions" :currentPage="currentPage" />
+    </div>
+
+    <div v-else>
+      <p class="text-center">Сессии не найдены.</p>
+    </div>
+
+    <div>
+      <q-pagination
+        v-if="totalSessions > sessionsPerPage"
+        v-model="currentPage"
+        :max="Math.ceil(totalSessions / sessionsPerPage)"
+        class="q-pa-xs"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.q-mt-md {
+  margin-top: 20px;
+}
+</style>
