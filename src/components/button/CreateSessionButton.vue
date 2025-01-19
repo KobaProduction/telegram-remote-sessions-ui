3a4b97d4def33aacd976791fba155695d5936f4b
Serent <Serent2003@mail.ru>
2025-01-19 15:47:33 +0300

    add button for auto create session

---
+++ src/components/button/CreateSessionButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import { createSession } from 'src/services/sessionsService';
+
+const createNewSession = async () => {
+  try {
+    const name = "fffff"; // Можно изменить логику создания имени
+    await createSession(name);
+    console.log('Сессия создана');
+  } catch (error) {
+    console.error('Ошибка при создании сессии:', error);
+  }
+};
+</script>
+
+<template>
+  <div class="col self-center">
+    <q-btn
+      @click="createNewSession"
+      color="primary"
+      label="Создать сессию"
+    />
+  </div>
+</template>
