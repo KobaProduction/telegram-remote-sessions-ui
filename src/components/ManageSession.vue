75fd7c1aa625ac1c27ff575d7a8e876ca1c351b5
Serent <Serent2003@mail.ru>
2025-01-19 21:01:30 +0300

    refactor: add api services usage

--- src/components/ManageSession.vue
+++ src/components/ManageSession.vue
@@ -1,39 +1,35 @@
 <script setup lang="ts">
 import { ref, defineProps, onMounted, defineEmits } from 'vue';
-import { getSessionDetails, deleteSession } from 'src/services/sessionsService';
+import { api } from 'src/services/trsApiInstance';
+import type { Session } from 'src/services/trsApiTypes';
 
-// Принимаем props
 const props = defineProps<{ sessionName: string | null }>();
 const emit = defineEmits(['close']);
 
-// Данные сессии
-const sessionDetails = ref<{ name: string; createdAt: string; status: string } | null>(null);
+const sessionDetails = ref<Session | null>(null);
 
-// Получаем детали сессии
 const fetchSessionDetails = async () => {
   if (props.sessionName) {
     try {
-      sessionDetails.value = await getSessionDetails(props.sessionName);
+      sessionDetails.value = await api.getSessionDetails(props.sessionName)
     } catch (error) {
       console.error('Ошибка при загрузке данных сессии:', error);
     }
   }
 };
 
-// Удаление сессии
 const deleteSessionHandler = async () => {
   if (props.sessionName) {
     try {
-      await deleteSession(props.sessionName);
+      await api.deleteSession(props.sessionName);
       console.log('Сессия удалена');
-      emit('close'); // Закрываем диалог
+      emit('close');
     } catch (error) {
       console.error('Ошибка при удалении сессии:', error);
     }
   }
 };
 
-// Загружаем данные при открытии
 onMounted(() => {
   fetchSessionDetails();
 });
@@ -43,8 +39,6 @@ onMounted(() => {
   <q-card v-if="sessionDetails" class="q-pa-md">
     <q-card-section>
       <h3>Сессия: {{ sessionDetails.name }}</h3>
-      <p>Создана: {{ sessionDetails.createdAt }}</p>
-      <p>Статус: {{ sessionDetails.status }}</p>
     </q-card-section>
 
     <q-card-actions>
