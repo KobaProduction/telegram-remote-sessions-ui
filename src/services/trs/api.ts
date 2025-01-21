a1aad8992b10cfb581b6ae73bd8a4afb847ad5e9
ArthurKoba <caprimores@gmail.com>
2025-01-21 22:57:21 +0300

    refactor api

--- src/services/trs/api.ts
+++ src/services/trs/api.ts
@@ -1,64 +1,59 @@
 import axios from 'axios'
-import type { AxiosResponse } from 'axios'
+import type {AxiosResponse } from 'axios'
 import type {
-  ServerStatusResponse,
   Session,
   SessionListResponse,
   SessionParameters,
-  ApiResponse
+  ApiResponse, ServerStatus
 } from './models'
 
 export class TrsApi {
-  private readonly url: string
+  private readonly url: URL
 
-  constructor(url: string) {
-    this.url = url
+  constructor(url: string = "http://localhost") {
+    this.url = new URL(url)
   }
 
-  private async get<T>(endpoint: string): Promise<T> {
-    const response: AxiosResponse<T> = await axios.get(`${this.url}${endpoint}`)
-    return response.data
+  private async get<T>(endpoint: string): Promise<AxiosResponse<T>> {
+    return await axios.get(`${this.url}${endpoint}`)
   }
 
-  private async post<T>(endpoint: string, data?: object): Promise<T> {
-    const response: AxiosResponse<T> = await axios.post(`${this.url}${endpoint}`, data)
-    return response.data
+  private async post<T>(endpoint: string, data?: object): Promise<AxiosResponse<T>> {
+    return await axios.post(`${this.url}${endpoint}`, data)
   }
 
-  private async put<T>(endpoint: string): Promise<T> {
-    const response: AxiosResponse<T> = await axios.put(`${this.url}${endpoint}`)
-    return response.data
+  private async put<T>(endpoint: string): Promise<AxiosResponse<T>> {
+    return await axios.put(`${this.url}${endpoint}`)
   }
 
-  private async delete<T>(endpoint: string): Promise<T> {
+  private async delete<T>(endpoint: string): Promise<AxiosResponse<T>> {
     return await axios.delete(`${this.url}${endpoint}`)
   }
 
-  async getStatus(): Promise<string> {
-    const response: ServerStatusResponse = await this.get('status')
-    return response.status
+  async getStatus(): Promise<ServerStatus> {
+    return (await this.get('v1/status')).data as ServerStatus
   }
 
   async updateSession(name: string, isActive: boolean | null = null, proxy: string | null = null): Promise<Session | undefined> {
     let url = `v1/session/update?name=${encodeURIComponent(name)}`
     if (isActive !== null) url += `&active=${encodeURIComponent(isActive)}`
     if (proxy !== null) url += `&proxy=${encodeURIComponent(proxy)}`
-    return await this.put<Session>(url)
+    return (await this.put<Session>(url)).data as Session
   }
 
   async getSessions(): Promise<SessionListResponse> {
-    return this.get(`v1/sessions/read`)
+    return (await this.get(`v1/sessions/read`)).data as SessionListResponse
   }
 
   async getSessionDetails(sessionName: string): Promise<Session> {
-    return this.get<Session>(`v1/session/read?name=${sessionName}`)
+    return (await this.get<Session>(`v1/session/read?name=${sessionName}`)).data as Session
   }
 
   async deleteSession(sessionName: string): Promise<ApiResponse> {
-      return this.delete(`v1/session/delete?name=${sessionName}`);
+      return (await this.delete(`v1/session/delete?name=${sessionName}`)).data as ApiResponse;
   }
 
   async createSession(name: string, sessionData: SessionParameters): Promise<ApiResponse> {
-    return this.post(`v1/session/create?name=${name}`, sessionData)
+    return (await this.post(`v1/session/create?name=${name}`, sessionData)).data as ApiResponse
   }
 }
--- src/services/trs/models.ts
+++ src/services/trs/models.ts
@@ -1,50 +1,50 @@
-
 export interface SessionParameters {
-  api_id: number;
-  api_hash: string;
-  app_version: string;
-  lang_code: string;
-  device_model: string;
-  system_version: string;
-  system_lang_code: string;
+  api_id: number
+  api_hash: string
+  app_version: string
+  lang_code: string
+  device_model: string
+  system_version: string
+  system_lang_code: string
+}
+
+export interface ServerStatus {
+  status: boolean
+  version: string
+  name: string
 }
 
 export interface Session {
-  name: string;
   is_active: boolean
-  is_authenticated: boolean;
-  is_broken: boolean;
-  proxy: string | null;
-  session_parameters: SessionParameters;
+  is_authenticated: boolean
+  is_broken: boolean
+  proxy: string | null
+  session_parameters: SessionParameters
 }
 
 export interface FullSessionData {
-  status: string;
-  message: string;
-  name?: string;
-  is_active?: boolean;
-  is_authenticated?: boolean;
-  is_broken?: boolean;
+  status: string
+  message: string
+  name?: string
+  is_active?: boolean
+  is_authenticated?: boolean
+  is_broken?: boolean
   session_parameters?: {
-    app_version: string;
-    lang_code: string;
-    device_model: string;
-    system_version: string;
-    system_lang_code: string;
-    api_id: number;
-    api_hash: string;
-  };
+    app_version: string
+    lang_code: string
+    device_model: string
+    system_version: string
+    system_lang_code: string
+    api_id: number
+    api_hash: string
+  }
 }
 
 export interface SessionListResponse {
-  sessions: string[];
-}
-
-export interface ServerStatusResponse {
-  status: string;
+  sessions: string[]
 }
 
 export interface ApiResponse {
-  status: boolean;
-  message: string;
+  status: boolean
+  message: string
 }
