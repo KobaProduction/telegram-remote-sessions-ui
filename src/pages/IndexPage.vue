b4997091a2aa1e5d1323f8a7db22be2f91328159
Serent <Serent2003@mail.ru>
2025-01-31 21:25:22 +0300

    ref: refactor ServerHistory to ServerDashboard

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 
 import ServerList from 'components/server/ServerList.vue'
-import { ServerHistory } from 'src/widgets/ServerHistory'
+import { ServerDashboard } from 'src/widgets/ServerHistory'
 </script>
 
 <template>
   <q-page class="row items-center justify-evenly">
     <ServerList/>
-    <ServerHistory/>
+    <ServerDashboard/>
   </q-page>
 </template>
