bf600bf0fae5fd0800946aa5d68c438352ffadeb
Serent <Serent2003@mail.ru>
2025-02-17 16:34:19 +0300

    chore(model.ts): remove useless interface

--- src/shared/api/trs/model.ts
+++ src/shared/api/trs/model.ts
@@ -22,23 +22,6 @@ export interface Session {
   session_parameters: SessionParameters
 }
 
-export interface FullSessionData {
-  status: string
-  message: string
-  name?: string
-  is_active?: boolean
-  is_authenticated?: boolean
-  is_broken?: boolean
-  session_parameters?: {
-    app_version: string
-    lang_code: string
-    device_model: string
-    system_version: string
-    system_lang_code: string
-    api_id: number
-    api_hash: string
-  }
-}
 
 export interface SessionListResponse {
   sessions: string[]
